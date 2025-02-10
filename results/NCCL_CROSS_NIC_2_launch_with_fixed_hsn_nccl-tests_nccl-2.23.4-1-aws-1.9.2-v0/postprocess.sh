94fd21e6c4159c9763662be58953bec0dedf1268
boeschf <48126478+boeschf@users.noreply.github.com>
2025-02-10 13:43:43 +0100

    results

---
+++ results/NCCL_CROSS_NIC_2_launch_with_fixed_hsn_nccl-tests_nccl-2.23.4-1-aws-1.9.2-v0/postprocess.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+/iopsstor/scratch/cscs/boeschf/nccl-testing-new/parse_output.sh "./job-n-00004-N-0001-151168.out" "./job-n-00004-N-0001"
+/iopsstor/scratch/cscs/boeschf/nccl-testing-new/parse_output.sh "./job-n-00008-N-0002-151169.out" "./job-n-00008-N-0002"
+/iopsstor/scratch/cscs/boeschf/nccl-testing-new/parse_output.sh "./job-n-00016-N-0004-151170.out" "./job-n-00016-N-0004"
+/iopsstor/scratch/cscs/boeschf/nccl-testing-new/parse_output.sh "./job-n-00032-N-0008-151171.out" "./job-n-00032-N-0008"
+/iopsstor/scratch/cscs/boeschf/nccl-testing-new/parse_output.sh "./job-n-00064-N-0016-151172.out" "./job-n-00064-N-0016"
+/iopsstor/scratch/cscs/boeschf/nccl-testing-new/parse_output.sh "./job-n-00128-N-0032-151173.out" "./job-n-00128-N-0032"
+/iopsstor/scratch/cscs/boeschf/nccl-testing-new/parse_output.sh "./job-n-00256-N-0064-151174.out" "./job-n-00256-N-0064"
+/iopsstor/scratch/cscs/boeschf/nccl-testing-new/parse_output.sh "./job-n-00512-N-0128-151175.out" "./job-n-00512-N-0128"
+/iopsstor/scratch/cscs/boeschf/nccl-testing-new/parse_output.sh "./job-n-01024-N-0256-151176.out" "./job-n-01024-N-0256"
+/iopsstor/scratch/cscs/boeschf/nccl-testing-new/parse_output.sh "./job-n-02048-N-0512-151177.out" "./job-n-02048-N-0512"
